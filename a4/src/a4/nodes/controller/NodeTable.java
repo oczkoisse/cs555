97d33d9ff60eeb9cee4ad024fb80da805ca52a5c
Rahul Bangar <rahul.bangar@colostate.edu>
2017-11-07 10:23:34 -0700

    NodeTable is thread safe

--- a4/src/a4/nodes/controller/NodeTable.java
+++ a4/src/a4/nodes/controller/NodeTable.java
@@ -25,10 +25,12 @@ public class NodeTable
         validateAddress(listeningAddress);
         validateFreeSpace(freeSpace);
 
-        if(nodes.containsKey(listeningAddress))
-            throw new IllegalArgumentException("Cannot add already existing node: " + listeningAddress);
-
-        nodes.put(listeningAddress, freeSpace);
+        synchronized (nodes)
+        {
+            if(nodes.containsKey(listeningAddress))
+                throw new IllegalArgumentException("Cannot add already existing node: " + listeningAddress);
+            nodes.put(listeningAddress, freeSpace);
+        }
     }
 
     public void updateNode(InetSocketAddress listeningAddress, long freeSpace)
@@ -36,40 +38,54 @@ public class NodeTable
         validateAddress(listeningAddress);
         validateFreeSpace(freeSpace);
 
-        if(!nodes.containsKey(listeningAddress))
-            throw new IllegalArgumentException("Cannot update a non-existing node: " + listeningAddress);
+        synchronized (nodes)
+        {
+            if(!nodes.containsKey(listeningAddress))
+               throw new IllegalArgumentException("Cannot update a non-existing node: " + listeningAddress);
 
-        nodes.put(listeningAddress, freeSpace);
+            nodes.put(listeningAddress, freeSpace);
+        }
     }
 
     public void removeNode(InetSocketAddress listeningAddress)
     {
         validateAddress(listeningAddress);
 
-        if(!nodes.containsKey(listeningAddress))
-            throw new IllegalArgumentException("Cannot remove a non-existing node: " + listeningAddress);
-
-        nodes.remove(listeningAddress);
-
-        // Also remove that node wherever it occurs as a replica
-        for(Map<Long, Set<InetSocketAddress>> map: chunkReplicas.values())
+        synchronized (nodes)
         {
-            // Relies on the fact that each chunk has unique set of replicas
-            for(Set<InetSocketAddress> set: map.values())
-                set.remove(listeningAddress);
+            synchronized (chunkReplicas)
+            {
+                if(!nodes.containsKey(listeningAddress))
+                    throw new IllegalArgumentException("Cannot remove a non-existing node: " + listeningAddress);
+                nodes.remove(listeningAddress);
+
+                // Also remove that node wherever it occurs as a replica
+                for(Map<Long, Set<InetSocketAddress>> map: chunkReplicas.values())
+                {
+                    // Relies on the fact that each chunk has unique set of replicas
+                    for(Set<InetSocketAddress> set: map.values())
+                        set.remove(listeningAddress);
+                }
+            }
         }
     }
 
     public Set<InetSocketAddress> getAllNodes()
     {
-        return Collections.unmodifiableSet(nodes.keySet());
+        synchronized (nodes)
+        {
+            return Collections.unmodifiableSet(nodes.keySet());
+        }
     }
 
     public boolean hasNode(InetSocketAddress listeningAddress)
     {
         validateAddress(listeningAddress);
 
-        return nodes.containsKey(listeningAddress);
+        synchronized (nodes)
+        {
+            return nodes.containsKey(listeningAddress);
+        }
     }
 
     public void addReplica(String filename, long sequenceNum, InetSocketAddress listeningAddress)
@@ -78,14 +94,17 @@ public class NodeTable
         validateSequenceNum(sequenceNum);
         validateAddress(listeningAddress);
 
-        chunkReplicas.putIfAbsent(filename, new HashMap<>());
+        synchronized (chunkReplicas)
+        {
+            chunkReplicas.putIfAbsent(filename, new HashMap<>());
 
-        chunkReplicas.get(filename).putIfAbsent(sequenceNum, new HashSet<>());
+            chunkReplicas.get(filename).putIfAbsent(sequenceNum, new HashSet<>());
 
-        if (!chunkReplicas.get(filename).get(sequenceNum).contains(listeningAddress))
-            chunkReplicas.get(filename).get(sequenceNum).add(listeningAddress);
-        else
-            throw new IllegalArgumentException("Attempt to add duplicate replica for same chunk");
+            if (!chunkReplicas.get(filename).get(sequenceNum).contains(listeningAddress))
+                chunkReplicas.get(filename).get(sequenceNum).add(listeningAddress);
+            else
+                throw new IllegalArgumentException("Attempt to add duplicate replica for same chunk");
+        }
     }
 
     public Set<InetSocketAddress> getAllReplicas(String filename, long sequenceNum)
@@ -93,9 +112,12 @@ public class NodeTable
         validateFileName(filename);
         validateSequenceNum(sequenceNum);
 
-        if(chunkReplicas.containsKey(filename) && chunkReplicas.get(filename).containsKey(sequenceNum)
-                && chunkReplicas.get(filename).get(sequenceNum).size() > 0)
-            return Collections.unmodifiableSet(chunkReplicas.get(filename).get(sequenceNum));
+        synchronized (chunkReplicas)
+        {
+            if(chunkReplicas.containsKey(filename) && chunkReplicas.get(filename).containsKey(sequenceNum)
+                    && chunkReplicas.get(filename).get(sequenceNum).size() > 0)
+                return Collections.unmodifiableSet(chunkReplicas.get(filename).get(sequenceNum));
+        }
         return null;
     }
 
@@ -105,16 +127,17 @@ public class NodeTable
         validateSequenceNum(sequenceNum);
 
         // For reading
-        Set<InetSocketAddress> replicas = getAllReplicas(filename, sequenceNum);
-        if (replicas != null)
+        synchronized (chunkReplicas)
         {
-            int r = random.nextInt(replicas.size());
-            int i = 0;
-            for(InetSocketAddress address: replicas)
-            {
-                if (i == r)
-                    return address;
-                i++;
+            Set<InetSocketAddress> replicas = getAllReplicas(filename, sequenceNum);
+            if (replicas != null) {
+                int r = random.nextInt(replicas.size());
+                int i = 0;
+                for (InetSocketAddress address : replicas) {
+                    if (i == r)
+                        return address;
+                    i++;
+                }
             }
         }
         return null;
@@ -125,30 +148,34 @@ public class NodeTable
         if (count <= 0)
             throw new IllegalArgumentException("Requested replica count must be a positive integer");
 
-        Set<InetSocketAddress> nodesAddresses = nodes.keySet();
-
-        // If not enough nodes are up yet, return null
-        if (nodesAddresses.size() < count)
-            return null;
-
         Set<InetSocketAddress> candidates = new HashSet<>();
 
-        while (candidates.size() < count)
+        synchronized (nodes)
         {
-            int n = random.nextInt(nodes.size());
-            int i = 0;
-            for(InetSocketAddress address: nodesAddresses)
+            Set<InetSocketAddress> nodesAddresses = nodes.keySet();
+
+            // If not enough nodes are up yet, return null
+            if (nodesAddresses.size() < count)
+                return null;
+
+
+            while (candidates.size() < count)
             {
-                if (i == n)
+                int n = random.nextInt(nodes.size());
+                int i = 0;
+                for(InetSocketAddress address: nodesAddresses)
                 {
-                    if(!candidates.contains(address))
-                        candidates.add(address);
-                    break;
+                    if (i == n)
+                    {
+                        if(!candidates.contains(address))
+                            candidates.add(address);
+                        break;
+                    }
+                    i++;
                 }
-                i++;
             }
+            return candidates;
         }
-        return candidates;
     }
 
     private void validateFileName(String filename)
